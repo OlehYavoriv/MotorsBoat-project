b69fcdbc6dc62a3888e02b524c038e836aa2e51e
OlehYavoriv <oleh.yavoriv@gmail.com>
2021-10-31 22:04:40 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -7,21 +7,6 @@ function openProduct(productName) {
   document.getElementById(productName).style.display = "block";
 }
 
-document.forms.form.onsubmit = function () {
-  let name = this.name.value;
-  let email = this.email.value;
-  let message = this.message.value;
-
-  if (message == 0 || name == 0 || email == 0) {
-    alert("Error! Please fill in all fields...");
-    return 1;
-  } else {
-    alert("Thank you! Your message has been sent, please reply... :)");
-  }
-  console.log("You have a new message!\n" + "Name: " + name + "\n" + "Email: " + email + "\n" + "Message: " + message);
-  return false;
-}
-
 $(document).on('click', '[data-toggle="modal"]', function () {
   var target = $(this).attr('data-target');
   console.log(target);
